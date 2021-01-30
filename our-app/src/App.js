eba9953368b7ca63548c5bbc944c044280a4f582
MG <michal.gulczynski93@gmail.com>
2021-01-30 22:12:31 +0100

    Handling basic events in React

--- our-app/src/App.js
+++ our-app/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Card from "./Card";
+import InputEvents from "./events/InputEvents";
+import MovementEvents from "./events/MovementEvents";
 
 
 function App() {
   return (
     <div className="App">
-        <Card />
-        <Card />
-        <Card />
+        <Card/>
+        <InputEvents></InputEvents>
+        <MovementEvents></MovementEvents>
     </div>
   );
 }
