dcdfbe9b94e4ae8a2398721ee6060187c9024450
Michał <mwarioxin@gmail.com>
2021-02-05 23:55:40 +0100

    Created Simple Shopping list app with saving data to local storage

--- our-app/src/App.js
+++ our-app/src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import Card from "./Card";
 import InputEvents from "./events/InputEvents";
 import MovementEvents from "./events/MovementEvents";
-
-
+import FormValidation from "./FormValidation";
+import ShoppingList from "./shoppingList/ShoppingList";
 function App() {
   return (
     <div className="App">
         <Card/>
         <InputEvents></InputEvents>
         <MovementEvents></MovementEvents>
+        <FormValidation></FormValidation>
+        <ShoppingList></ShoppingList>
     </div>
   );
 }
